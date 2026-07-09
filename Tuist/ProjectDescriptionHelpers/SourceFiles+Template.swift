9effe4644177a8d17ea6203c251239ec6027ffc6
Jeon0976 <jsh097610@gmail.com>
2026-07-09 11:50:25 +0900

    🧱 [Feat]: Tuist ProjectDescriptionHelpers 셋팅

--- Tuist/ProjectDescriptionHelpers/SourceFiles+Template.swift
+++ Tuist/ProjectDescriptionHelpers/SourceFiles+Template.swift
@@ -5,3 +5,12 @@
 //  Created by 전성훈 on 7/8/26.
 //
 
+import ProjectDescription
+
+public extension SourceFilesList {
+    static var interface: SourceFilesList { "Interface/Sources/**" }
+    static var implementation: SourceFilesList { "Sources/**" }
+    static var testing: SourceFilesList { "Testing/Sources/**" }
+    static var unitTests: SourceFilesList { "Tests/**" }
+    static var demo: SourceFilesList { "Demo/Sources/**" }
+}
--- Tuist/ProjectDescriptionHelpers/Target+MicroFeature.swift
+++ Tuist/ProjectDescriptionHelpers/Target+MicroFeature.swift
@@ -5,3 +5,12 @@
 //  Created by 전성훈 on 7/8/26.
 //
 
+import DependencyPlugin
+import EnvironmentPlugin
+import ProjectDescription
+
+public extension Target {
+    // 1. Interface - 이 모듈에 바깥에 보여준다.
+    static func interface(module: ModulePaths, dependencies: [TargetDependency] = []) -> Target {
+    }
+}
